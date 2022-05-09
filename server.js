a98c128179e7ed30915b52911dbe448920482fc6
noobkyle <kadangotinaishe5@gmail.com>
2022-05-09 13:49:24 -0500

    Working API with Create & Read

--- server.js
+++ server.js
@@ -12,14 +12,21 @@ app.use(bodyParser.json()); //parse json
 app.use(cors());  //cors for all requests.
 app.use(morgan());  //requests logging
 
-const ads = [
-  {title: "hello, world"}
+var ads = [
+  {address: "8655 Brookhollow Blvd, Frisco, Texas", cost: "$860 000", bedrooms: "4", squarefeet: 1600, garden: true, pool: false },
+  {address: "47 Dawnhill Rd, Aubrey, Texas", cost: "$300 000", bedrooms: "1", squarefeet: 600, garden: false, pool: false }
 ]
 
 //Routes
-app.get('/', (req,res) => {
+app.get('/properties', (req,res) => {
   res.send(ads);
 });
+
+app.post('/properties/create', (req, res) => {
+  console.log(req.body);
+  ads.push(req.body);
+  res.sendStatus(200);
+})
 
 //Start Server
 app.listen(3001, () => {
